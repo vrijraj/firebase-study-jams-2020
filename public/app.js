d6ef8c9fb965e94725d762fe1b2b142342bc5b79
Vrijraj Singh <vrijraj2396@gmail.com>
2020-02-18 12:41:58 +0530

    first commit

---
+++ public/app.js
@@ -0,0 +1,12 @@
+// Your web app's Firebase configuration
+var firebaseConfig = {
+    apiKey: "AIzaSyBuMHJWesNpDJPHWDsVRQ5GtGH1fPKCpAA",
+    authDomain: "firestudyjam.firebaseapp.com",
+    databaseURL: "https://firestudyjam.firebaseio.com",
+    projectId: "firestudyjam",
+    storageBucket: "firestudyjam.appspot.com",
+    messagingSenderId: "898928305925",
+    appId: "1:898928305925:web:119a4cb0b6ece97ccd796c"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
